9448bce925ca0443e63924c142ba799839d063c7
Sigurd015 <yujiang015@gmail.com>
2023-05-24 23:34:48 +0900

    Remove Sandbox Test

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -8,79 +8,20 @@
 
 class ExampleLayer :public Hanabi::Layer
 {
-private:
-	glm::vec2 m_ViewportSize = { 0.0f, 0.0f };
-	glm::vec2 m_ViewportBounds[2];
-	Hanabi::Ref<Hanabi::Framebuffer> m_Framebuffer;
-	Hanabi::EditorCamera m_EditorCamera;
 public:
 	ExampleLayer() :Layer("ExampleLayer") {}
 	~ExampleLayer() {}
 	void OnAttach() override
 	{
-		Hanabi::FramebufferSpecification fbSpec;
-		fbSpec.Attachments = { Hanabi::FramebufferTextureFormat::RGBA8, Hanabi::FramebufferTextureFormat::RED_INTEGER ,Hanabi::FramebufferTextureFormat::Depth };
-		fbSpec.Width = 1920;
-		fbSpec.Height = 1080;
-		m_Framebuffer = Hanabi::Framebuffer::Create(fbSpec);
 	}
 	void OnDetach() override {}
 	void OnUpdate(Hanabi::Timestep ts)override
 	{
-		// Resize
-		if (Hanabi::FramebufferSpecification spec = m_Framebuffer->GetSpecification();
-			m_ViewportSize.x > 0.0f && m_ViewportSize.y > 0.0f && // zero sized framebuffer is invalid
-			(spec.Width != m_ViewportSize.x || spec.Height != m_ViewportSize.y))
-		{
-			m_Framebuffer->Resize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
-			m_EditorCamera.SetViewportSize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
-		}
-
-		m_EditorCamera.OnUpdate(ts);
-
-		m_Framebuffer->Bind();
-		Hanabi::RenderCommand::SetClearColor({ 0.3f,0.3f,0.3f,1.0f });
-		Hanabi::RenderCommand::Clear();
-		m_Framebuffer->ClearAttachment(1, -1);
-
-		Hanabi::Renderer2D::ResetStats();
-		Hanabi::Renderer2D::BeginScene(m_EditorCamera);
-
-		for (float y = -5.0f; y < 5.0f; y += 0.5f)
-		{
-			for (float x = -5.0f; x < 5.0f; x += 0.5f)
-			{
-				glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.7f };
-				Hanabi::Renderer2D::DrawQuad(glm::translate(glm::mat4(1.0f), { x * 2.5f,y * 2.5f,1.0f }), color, 5);
-				Hanabi::Renderer2D::DrawCircle(glm::translate(glm::mat4(1.0f), { x * 3,y * 3,1.0f }), color, 1.0f, 0.005f, 6);
-				Hanabi::Renderer2D::DrawRect(glm::translate(glm::mat4(1.0f), { x * 5,y * 5,1.0f }), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
-			}
-		}
-		Hanabi::Renderer2D::EndScene();
-
-		ImVec2 mousePos = ImGui::GetMousePos();
-		mousePos.x -= m_ViewportBounds[0].x;
-		mousePos.y -= m_ViewportBounds[0].y;
-		glm::vec2 viewportSize = m_ViewportBounds[1] - m_ViewportBounds[0];
-		mousePos.y = viewportSize.y - mousePos.y;
-		int mouseX = (int)mousePos.x;
-		int mouseY = (int)mousePos.y;
-
-		if (mouseX >= 0 && mouseY >= 0 && mouseX < (int)viewportSize.x && mouseY < (int)viewportSize.y)
-		{
-			int pixelData = m_Framebuffer->ReadPixel(1, mouseX, mouseY);
-			HNB_INFO("Mouse X{0},Y{1},Selected:{2}", mouseX, mouseY, pixelData);
-		}
-
-		m_Framebuffer->Unbind();
-
-		//Hanabi_INFO("Timestep:",ts);
 		//HNB_INFO("Time Step:{0}", ts);
 	}
 
 	void OnEvent(Hanabi::Event& evnet) override
 	{
-		m_EditorCamera.OnEvent(evnet);
 		Hanabi::EventDispatcher dispatcher(evnet);
 		//dispatcher.Dispatch<Hanabi::KeyPressedEvent>(HNB_BIND_EVENT_FN(ExampleLayer::OnKeyPressed));
 		//dispatcher.Dispatch<Hanabi::KeyReleasedEvent>(HNB_BIND_EVENT_FN(ExampleLayer::OnKeyReleased));
@@ -92,77 +33,6 @@ public:
 	}
 	void OnImGuiRender()override
 	{
-		static bool dockspaceOpen = true;
-		static bool opt_fullscreen_persistant = true;
-		bool opt_fullscreen = opt_fullscreen_persistant;
-		static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
-
-		// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
-		// because it would be confusing to have two docking targets within each others.
-		ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-		if (opt_fullscreen)
-		{
-			ImGuiViewport* viewport = ImGui::GetMainViewport();
-			ImGui::SetNextWindowPos(viewport->Pos);
-			ImGui::SetNextWindowSize(viewport->Size);
-			ImGui::SetNextWindowViewport(viewport->ID);
-			ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-			ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-			window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-			window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-		}
-
-		// When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background and handle the pass-thru hole, so we ask Begin() to not render a background.
-		if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-			window_flags |= ImGuiWindowFlags_NoBackground;
-
-		// Important: note that we proceed even if Begin() returns false (aka window is collapsed).
-		// This is because we want to keep our DockSpace() active. If a DockSpace() is inactive, 
-		// all active windows docked into it will lose their parent and become undocked.
-		// We cannot preserve the docking relationship between an active window and an inactive docking, otherwise 
-		// any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-		ImGui::Begin("DockSpace Demo", &dockspaceOpen, window_flags);
-		ImGui::PopStyleVar();
-
-		if (opt_fullscreen)
-			ImGui::PopStyleVar(2);
-
-		// DockSpace
-		ImGuiIO& io = ImGui::GetIO();
-		ImGuiStyle& style = ImGui::GetStyle();
-		float minWinSizeX = style.WindowMinSize.x;
-		style.WindowMinSize.x = 370.0f;
-		if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
-		{
-			ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-			ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-		}
-		style.WindowMinSize.x = minWinSizeX;
-
-		ImGui::Begin("Settings");
-		auto stats = Hanabi::Renderer2D::GetStats();
-		ImGui::Text("Renderer2D Stats:");
-		ImGui::Text("Draw Calls: %d", stats.DrawCalls);
-		ImGui::Text("Quads: %d", stats.QuadCount);
-		ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
-		ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
-		ImGui::End();
-
-		ImGui::Begin("Viewport");
-
-		ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
-		m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
-		ImGui::Image(m_Framebuffer->GetColorAttachment(), ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
-
-		auto viewportMinRegion = ImGui::GetWindowContentRegionMin();
-		auto viewportMaxRegion = ImGui::GetWindowContentRegionMax();
-		auto viewportOffset = ImGui::GetWindowPos();
-		m_ViewportBounds[0] = { viewportMinRegion.x + viewportOffset.x, viewportMinRegion.y + viewportOffset.y };
-		m_ViewportBounds[1] = { viewportMaxRegion.x + viewportOffset.x, viewportMaxRegion.y + viewportOffset.y };
-
-		ImGui::End();
-		ImGui::End();
 	}
 
 	bool OnMouseMove(Hanabi::MouseMovedEvent& event)
