d4b530a500f5ebc97fed71571c1c168299090647
Sigurd015 <yujiang015@gmail.com>
2023-05-31 18:38:42 +0900

    Tracking Sandbox Project

---
+++ Hanabi_Editor/SandboxProject/Assets/Scripts/Source/Player.cs
@@ -0,0 +1,49 @@
+﻿using Hanabi;
+using System;
+using System.Threading;
+
+namespace Sandbox
+{
+    public class Player : Entity
+    {
+        private TransformComponent m_Transform;
+        private Rigidbody2DComponent m_Rigidbody;
+
+        public float Speed;
+        public float Time = 0.0f;
+
+        void OnCreate()
+        {
+            Log.Info($"Player.OnCreate - {ID}");
+            m_Transform = GetComponent<TransformComponent>();
+            m_Rigidbody = GetComponent<Rigidbody2DComponent>();
+        }
+
+        void OnUpdate(float ts)
+        {
+            Time += ts;
+
+            Log.Info($"Player.OnUpdate - {Time}");
+            Log.Warn($"Player.OnUpdate - {Time}");
+            Log.Trace($"Player.OnUpdate - {Time}");
+            Log.Critical($"Player.OnUpdate - {Time}");
+            Log.Error($"Player.OnUpdate - {Time}");
+
+            Vector3 velocity = Vector3.Zero;
+
+            if (Input.IsKeyDown(KeyCode.W))
+                velocity.Y = 1.0f;
+            else if (Input.IsKeyDown(KeyCode.S))
+                velocity.Y = -1.0f;
+
+            if (Input.IsKeyDown(KeyCode.A))
+                velocity.X = -1.0f;
+            else if (Input.IsKeyDown(KeyCode.D))
+                velocity.X = 1.0f;
+
+            velocity *= Speed * ts;
+
+            m_Rigidbody.ApplyLinearImpulse(velocity.XY, true);
+        }
+    }
+}
---
+++ Hanabi_Editor/SandboxProject/Assets/Scripts/premake5.lua
@@ -0,0 +1,50 @@
+workspace "Sandbox"
+	architecture "x86_64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+project "Sandbox"
+	kind "SharedLib"
+	language "C#"
+	dotnetframework "4.7.2"
+
+	targetdir ("Binaries")
+	objdir ("Binaries/Intermediates/")
+
+	files 
+	{
+		"Source/**.cs",
+		"Properties/**.cs"
+	}
+
+	links
+	{
+		"Hanabi_ScriptCore"
+	}
+	
+	filter "configurations:Debug"
+		optimize "Off"
+		symbols "Default"
+
+	filter "configurations:Release"
+		optimize "On"
+		symbols "Default"
+
+	filter "configurations:Dist"
+		optimize "Full"
+		symbols "Off"
+
+group "ScriptCore"
+	include ("../../../../Hanabi_ScriptCore")
+group ""
